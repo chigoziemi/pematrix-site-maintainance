d2360bc1049552614dcdd4f934818aba59f9187f
Chigoziem Iheanacho <chigoziemi@hotmail.com>
2023-12-07 18:04:23 +0100

    construction

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to Pematrix Technologies!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Our site is currently under construction <code>We will be back before you know it!</code>
         </p>
       </main>
 
